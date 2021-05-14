16982c3fcf61dccb00163d921d70050e7b60a8eb
thangPTIT <thang.tv.dragon@gmail.com>
2021-05-14 22:35:55 +0700

    frist commit

---
+++ src/test/java/com/thangtest/demo/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thangtest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
