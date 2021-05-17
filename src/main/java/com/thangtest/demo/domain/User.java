c97f400f5f200f71770232b3b70d55bc9b94e8bc
thangPTIT <thang.tv.dragon@gmail.com>
2021-05-17 12:58:53 +0700

    lombok

--- src/main/java/com/thangtest/demo/domain/User.java
+++ src/main/java/com/thangtest/demo/domain/User.java
@@ -1,5 +1,8 @@
 package com.thangtest.demo.domain;
 
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
 import java.util.Set;
 
@@ -15,6 +18,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "user")
+@Data
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -37,35 +41,4 @@ public class User implements Serializable {
     )
     private Set<Role> roles;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
